027fdb14637367e8f929851e330c6a3c6b0e29b5
Ricky Loader <loaderricky123@gmail.com>
2018-11-07 13:07:01 +1300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nFetching Grand Exchange data...\n\n");
+        ExchangeData exchange = new ExchangeData();
+        System.out.println("\nPlease enter an item name or item id:\n\n");
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine().toLowerCase();
+            System.out.println(exchange.requestItem(line));
+        }
+    }
+}
+
+
